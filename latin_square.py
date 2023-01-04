a406035cd2b16594afeff1d9959d5da6ee08dbce
AhmadRzKhalili <ahmadrkl2000@gmail.com>
2023-01-05 00:00:53 +0330

    MRV Heuristic fixed, Some bugs fixed

--- latin_square.py
+++ latin_square.py
@@ -1,46 +1,44 @@
 from random import randrange
 
-N = int(input('Enter n: '))
 
-
-def init_valid_values():
-    values = [*range(1, N + 1)]
+def init_valid_values(n):
+    values = [*range(1, n + 1)]
     valid_values = []
 
-    for i in range(N):
+    for i in range(n):
         row = []
-        for j in range(N):
+        for j in range(n):
             row.append(values)
 
         valid_values.append(row)
 
     return valid_values
 
-def init_latin_square():
+def init_latin_square(n):
     latin_square = []
 
-    for i in range(N):
-        row = [0 for j in range(1, N + 1)]
+    for i in range(n):
+        row = [0 for j in range(1, n + 1)]
         latin_square.append(row)
 
     return latin_square
 
-def is_valid(valid_values):
-    for i in  range(N):
-        for j in range(N):
+def is_valid(valid_values, n):
+    for i in  range(n):
+        for j in range(n):
             if len(valid_values[i][j]) == 0:
                 return False
     return True
 
-def update_valid_values(valid_values, number, row_index, col_index):
+def update_valid_values(valid_values, number, row_index, col_index, n):
     
-    for i in range(N):
+    for i in range(n):
         arr = valid_values[row_index][i].copy()
         if number in arr:
             arr.remove(number)
             valid_values[row_index][i] = arr
 
-    for i in range(N):
+    for i in range(n):
         arr = valid_values[i][col_index].copy()
         if number in arr:
             arr.remove(number)
@@ -50,30 +48,30 @@ def update_valid_values(valid_values, number, row_index, col_index):
 
     return valid_values 
 
-def update_mrv(valid_values):
+def update_mrv_heuristic(valid_values, n):
 
     mrv = []
 
 
-    for i in range(N):
+    for i in range(n):
         row = []
-        for j in range(N):
+        for j in range(n):
             if len(valid_values[i][j]) > 0:
                 row.append(len(valid_values[i][j]))
             else:
-                row.append(N)
+                row.append(n)
 
         mrv.append(row)
 
     return mrv
 
-def find_index_mrv(mrv_values):
-    mrv = N
+def find_index_mrv(mrv_values, n):
+    mrv = n
     row = 0
     col = 0
 
-    for i in range(N):
-        for j in  range(N):
+    for i in range(n):
+        for j in  range(n):
             if mrv_values[i][j] < mrv:
                 row = i
                 col = j
@@ -81,11 +79,11 @@ def find_index_mrv(mrv_values):
     
     return row, col
 
-def forward_checking(latin_square, valid_values):
+def forward_checking(latin_square, valid_values, n):
     
-    for i in range(N*N):
-        mrv_values = update_mrv(valid_values)
-        indices = find_index_mrv(mrv_values)
+    for i in range(n * n):
+        mrv_values = update_mrv_heuristic(valid_values, n)
+        indices = find_index_mrv(mrv_values, n)
         row = indices[0]
         col = indices[1]
 
@@ -93,20 +91,24 @@ def forward_checking(latin_square, valid_values):
         number = valid_values_arr[0]
         latin_square[row][col] = number
 
-        valid_values = update_valid_values(valid_values, number, row, col)
+        valid_values = update_valid_values(valid_values, number, row, col, n)
 
     return latin_square
 
-def print_solution(solution):
-    for i in range(N):
-        for j in range(N):
+def print_solution(solution, n):
+    for i in range(n):
+        for j in range(n):
             print(solution[i][j], end=" ")
 
         print()
 
 
-latin_square = init_latin_square()
-valid_values = init_valid_values()
-solution = forward_checking(latin_square, valid_values)
-print_solution(solution)
+def main():
+    n = int(input('Enter n: '))
+    latin_square = init_latin_square(n)
+    valid_values = init_valid_values(n)
+    solution = forward_checking(latin_square, valid_values, n)
+    print_solution(solution, n)
 
+if __name__ == "__main__":
+    main()
